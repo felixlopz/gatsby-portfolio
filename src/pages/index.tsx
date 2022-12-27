46fbad57952255fe59825c6166c54a795cdc7a74
Felix <felixlopzd@gmail.com>
2022-12-27 16:01:27 -0400

    tailwindcss and styled components setup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,18 @@
 import * as React from "react";
 import type { HeadFC, PageProps } from "gatsby";
+import styled from "styled-components";
+import tw from "twin.macro";
+
+const Title = styled.h1`
+  ${tw`text-3xl font-bold underline`}
+`;
 
 const IndexPage: React.FC<PageProps> = () => {
-  return <div>Hello this is my gatsby portfolio</div>;
+  return (
+    <div className="container">
+      <Title>Hello this is my gatsby portfolio</Title>
+    </div>
+  );
 };
 
 export default IndexPage;
